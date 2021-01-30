eaf258f844f94fc89b91437d8750bb756c538183
Yuting Lin <72534426+filizlin@users.noreply.github.com>
2021-01-30 08:00:58 +0800

    javascript assignment

---
+++ calculator.js
@@ -0,0 +1,11 @@
+let var1 = 4;
+let var2 = 5;
+let var3 = 6;
+let var4 = 7;
+
+console.log(var1 + var3);
+console.log(var2 - var3);
+console.log(var3 * var4);
+console.log(var1 / var4);
+console.log(var3 % var1);
+
