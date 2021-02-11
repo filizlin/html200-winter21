b7731c928095a2f2cd7605f5c05d0323901d4af3
Yuting Lin <72534426+filizlin@users.noreply.github.com>
2021-02-11 14:36:13 +0800

    Assignment 03

---
+++ Assignment03 - exercise-three.js
@@ -0,0 +1,6 @@
+//Use a for loop to console log all of the elements plus the index and a comma
+let array = [[1, 2, 3, 4, 5], ['one', 'two', 'three', 'four', 'five']];
+
+for (let j = 0; j < 5; j++) {
+    console.log(array[0][j] + ',' + array[1][j]);
+}
---
+++ Assignment03 - exercise-two.js
@@ -0,0 +1,6 @@
+//Set a string variable to be a sentence of at least five words and a period.
+let string ='Hello my name is Yuting Lin.';
+
+//Make an array from that sentence and console.log the third word.
+let array = string.split(' ');
+console.log(array[2])
