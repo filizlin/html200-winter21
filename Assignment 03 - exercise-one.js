1e5abbca2e035c64133428bbbaf3926b9fb6b8db
Yuting Lin <72534426+filizlin@users.noreply.github.com>
2021-02-11 13:31:16 +0800

    Assignment 03

---
+++ Assignment 03 - exercise-one.js
@@ -0,0 +1,16 @@
+//Create an array, with at least 5 strings
+let array = ['one', 'two', 'three', 'four','five'];
+console.log(array);
+
+//Add an element to the end of the array
+array.push('add this');
+console.log(array);
+
+//Remove the third element
+array.splice (2,1);
+console.log(array);
+
+
+//Create a string from the elements and comma separate them
+let string = array.join(',');
+console.log(array);
